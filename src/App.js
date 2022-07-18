b231d0ec05fc0c86170322e4de1c9ee436cb299a
Ebernauta <ebernauta18@gmail.com>
2022-07-17 22:04:51 -0400

    Agrego el useState y el nanoid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState } from 'react';
+import {nanoid} from 'nanoid';
+import ListasNotas from "./components/ListasNotas";
+
 const App = () => {
-  return(<p>Hola primer commit</p>)
+
+  const [notas, setNotas] = useState([
+    {
+      id: nanoid(),
+      titulo: "Esta es mi primera nota",
+      texto: "Esta es una prueba de mi nota",
+    },
+    {
+      id: nanoid(),
+      titulo: "Esta es mi segunda nota",
+      texto: "Esta es una prueba de mi nota",
+    },
+    {
+      id: nanoid(),
+      titulo: "Esta es mi tercera nota",
+      texto: "Esta es una prueba de mi nota",
+    },
+    {
+      id: nanoid(),
+      titulo: "Esta es mi nueva nota",
+      texto: "Esta es una prueba de mi nota",
+    }
+
+  ]);
+
+  return (
+    <div className="container">
+      <ListasNotas notas={notas}/>
+    </div>
+  );
 };
 
 export default App;
